f664eb68cfbf567c9f6da213c1b4489454c6fd61
yutopp <yutopp@gmail.com>
2019-08-13 02:34:00 +0900

    Fix segment size. Align segments

--- stone/self.asm
+++ stone/self.asm
@@ -1,6 +1,7 @@
     bits    64
     org     0x00400000          ; このあたりにマッピングしておきたい
 
+segment_align equ 0x100
 
 ;;; ELF Header
     ;; e_ident
@@ -28,7 +29,7 @@
     ;; e_phentsize, (56)
     dw 0x0038
     ;; e_phnum
-    dw 0x0001
+    dw 0x0002
     ;; e_shentsize, (64)
     dw 0x0040
     ;; e_shnum
@@ -40,6 +41,7 @@
 
 ;;; Program headers
 phdrs:
+    ;; [0]
     ;; p_type, PT_LOAD
     dd 0x00000001
     ;; p_flags, R(4)X(1)
@@ -51,20 +53,44 @@ phdrs:
     ;; p_paddr
     dq 0
     ;; p_filesz
-    dq 0x0000000000000010
+    dq rest_size
     ;; p_memsz
-    dq 0x0000000000000010
+    dq rest_size
     ;; p_align
+    dq segment_align
+
+    ;; [1]
+    ;; p_type, PT_LOAD
+    dd 0x00000001
+    ;; p_flags, R(4)X(1)
+    dd 0x00000005
+    ;; p_offset
     dq 0x0000000000001000
-
-
+    ;; p_vaddr,
+    dq $$+0x0000000000001000
+    ;; p_paddr
+    dq 0
+    ;; p_filesz
+    dq code_segment_rest_size
+    ;; p_memsz
+    dq code_segment_rest_size
+    ;; p_align
+    dq segment_align
 
 ;;; Segments
+    ;; Rest
+rest_size equ ($-$$)
+    align segment_align
+
+    ;; Code
+code_segment:
 _start:
     mov rax, 60
     mov rdi, 42
     syscall
 
+code_segment_rest_size equ ($-code_segment)
+    align segment_align
 
 ;;; Section headers
 shdrs:
